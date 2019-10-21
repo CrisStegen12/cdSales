8ea5c980ff3b4464bbcdb6a73327210a2779f424
CriStegen12 <cristianopereiraramos9@gmail.com>
2019-10-21 12:53:10 -0300

    final home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import { CadastroProdutosComponent } from './cadastro-produtos/cadastro-produtos.component';
 import { AdminComponent } from './admin/admin.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
 
 const routes: Routes = [
   { path: 'cadastro-produtos', component: CadastroProdutosComponent},
   { path: 'admin', component: AdminComponent },
+  {path: 'home', component: HomePageComponent}
 ];
 
 
